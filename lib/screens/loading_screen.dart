d2273710ad422f37f885e36646954995e77a8dd6
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-29 09:39:50 +0900

    add package http and sample openweathermap api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_course_clima/services/location.dart';
+
+const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,8 +21,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
   }
 
+  void getData() async {
+    Uri url = Uri.https(
+      'samples.openweathermap.org',
+      '/data/2.5/weather',
+      {
+        'lat': '35',
+        'lon': '139',
+        'appid': API_KEY,
+      },
+    );
+
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
         // body: Center(
         //   child: ElevatedButton(
